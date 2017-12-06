e98ef734f0dd77f574cfe19749ae557f6be2f455
Alex McKean <amckean@me.com>
2017-12-06 01:27:50 +0000

    automatically backed up by learn

--- lib/user.rb
+++ lib/user.rb
@@ -2,7 +2,7 @@ class User
   attr_accessor :firstname, :lastname
 
   def initialize(firstname, lastname)
-    @firstname = firstname
-    @lastname = lastname
+    @first_name = firstname
+    @last_name = lastname
   end
 end
